083c6ddc922d9d01662128ddeae0eca245147b79
grahamjantz <grahammrjantz@gmail.com>
2024-03-06 13:07:13 -0700

    work

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,40 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [currentDate, setCurrentDate] = useState('')
+  const [currentTime, setCurrentTime] = useState('')
+
+  // console.log(currentDate)
+
+  // console.log('11000111000010101001100011000001000110000'.length)
+
+  // console.log(Math.round(+new Date()/1000).toString(2).length)
+  
+  
+  setTimeout(() => {
+    const binTime = Math.floor(Date.now()/1000).toString(2)
+    
+    // console.log('Date.now():', Math.round(Date.now() / 1000).toString(2))
+    // console.log('parseInt:', parseInt(binTime))
+    // console.log('date:', Date(parseInt(binTime)))
+
+    const time = Date(parseInt(binTime))
+    setCurrentTime(time)
+
+    setCurrentDate(binTime)
+  },"1000")
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App w-[75vw] max-w-xl h-[75vh] flex flex-col items-center gap-4 font-mono'>
+      <h1 className=' mb-4'>Epochalypse</h1>
+      <p className='leading-none'>&nbsp; At <b>03:14:08 UTC on 19 January 2038</b> the signed 32-bit integer representing seconds from the Unix epoch (00:00:00 UTC on Jan 1, 1970) will rollover, potentially causing damage to systems relying on this number.</p>
+    
+      <hr />
+      <p>0{currentDate.slice(0,7)} {currentDate.slice(8,15)} {currentDate.slice(16,24)} {currentDate.slice(24,31)}</p>
+      <p>{new Date(currentTime).toLocaleString()}</p>
+    </div>
   )
 }
 
