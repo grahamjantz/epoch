1429d4441d430d45fe8ffad20530117edcb9ef82
grahamjantz <grahammrjantz@gmail.com>
2024-03-06 17:18:40 -0700

    added countdown

---
+++ src/Countdown.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+
+const Countdown = () => {
+  const [countdown, setCountdown] = useState('');
+
+  useEffect(() => {
+    const targetDate = new Date('2038-01-19T03:14:08Z');
+    
+    const updateCountdown = () => {
+      const currentDate = new Date();
+      const difference = targetDate - currentDate;
+
+      if (difference > 0) {
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+        setCountdown(`${days}d ${hours}h ${minutes}m ${seconds}s`);
+      } else {
+        setCountdown('Countdown expired');
+      }
+    };
+
+    const intervalId = setInterval(updateCountdown, 1000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className='countdown flex justify-center items-end'>
+      <p className='text-xl font-bold'>
+        Time remaining: <b className='text-xl text-red-400 font-bold '>{countdown}</b>
+      </p>
+    </div>
+  );
+};
+
+export default Countdown;
